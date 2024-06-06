c2bbaea23e65b2226a6c3f25f3f147b00279caf0
fahadkabali <kabalifahad@gmail.com>
2024-06-06 23:56:25 +0300

    update

--- Evaluations/settings.py
+++ Evaluations/settings.py
@@ -130,13 +130,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Assuming 'static' is at the root of your project
+]
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'core/static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
