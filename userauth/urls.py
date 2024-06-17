dee96ed23c061f873378317062233ec9eacee29b
fahadkabali <kabalifahad@gmail.com>
2024-06-17 21:15:06 +0300

    update

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-# app_name=('userauth')
+app_name=('userauth')
 urlpatterns = [
     path('', views.home, name='home'),
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
